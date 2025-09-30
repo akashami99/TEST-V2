d351718dcf607f15a6c3cc6ebcb505424930f959
akashami99 <arakashpersonal@gmail.com>
2025-09-30 20:30:08 +0600

    Update Autoreply.js

--- AKASH/commands/Autoreply.js
+++ AKASH/commands/Autoreply.js
@@ -1,36 +1,27 @@
-const fs = global.nodemodule["fs-extra"];
-const path = global.nodemodule["path"];
-
-module.exports.config = {
-  name: "autoreplybot",
-  version: "6.0.2",
-  hasPermssion: 0,
-  credits: "𝐒𝐡𝐚𝐡𝐚𝐝𝐚𝐭 𝐈𝐬𝐥𝐚𝐦",
-  description: "Auto-response bot with specified triggers",
-  commandCategory: "No Prefix",
-  usages: "[any trigger]",
-  cooldowns: 3,
-};
-
 module.exports.handleEvent = async function ({ api, event, Users }) {
-  const { threadID, messageID, senderID, body } = event;
+  const { threadID, messageID, senderID, body, attachments } = event;
+
+  // Messenger নীল লাইক (sticker)
+  if (attachments && attachments[0]?.type === "sticker") {
+    const likeStickers = ["369239263222821","369239343222803","369239343222804"];
+    if (likeStickers.includes(attachments[0].stickerID.toString())) {
+      return api.sendMessage("আবা*ল নাকি লাইক কেন ব্যা 😾🔪", threadID, messageID);
+    }
+  }
+
   if (!body) return; 
   const name = await Users.getNameUser(senderID);
   const msg = body.toLowerCase().trim();
 
   const responses = {
+    "hi": "এত হাই-হ্যালো কর ক্যান প্রিও..!😜🫵",
+    "help": "Prefix de sala",
     "miss you": "অরেক বেডারে Miss না করে xan মেয়ে হলে বস Akash রে হাঙ্গা করো😶👻😘",
     "kiss de": "কিস দিস না তোর মুখে দূর গন্ধ কয়দিন ধরে দাঁত ব্রাশ করিস নাই🤬",
-    "👍": "সর এখান থেকে লাইকার আবাল..!🐸🤣👍⛏️",
-    "help": "Prefix de sala",
-    "hi": "এত হাই-হ্যালো কর ক্যান প্রিও..!😜🫵",
     "😦": "হায়রে! 😦",
-};
+  };
+
   if (responses[msg]) {
     return api.sendMessage(responses[msg], threadID, messageID);
   }
-};
-
-module.exports.run = async function ({ api, event, args, Users }) {
-  return this.handleEvent({ api, event, Users });
 };
